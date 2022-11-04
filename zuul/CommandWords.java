a18910cb80ba30100937d54c637a99984a7fee0c
DarenKostov <daren.kostov@outlook.com>
2022-11-04 11:47:44 -0700

    removal-of-isCommand-(obsolete)

--- zuul/CommandWords.java
+++ zuul/CommandWords.java
@@ -39,23 +39,6 @@ class CommandWords
         // nothing to do at the moment...
     }
 
-    /**
-     * Check whether a given String is a valid command word. 
-     * Return true if it is, false if it isn't.
-     */
-    public boolean isCommand(String aString)
-    {
-        for(int i = 0; i < validCommands.length; i++) {
-            for(int j=0; j<validCommands[i].length; j++) {
-                if(validCommands[i][j].equals(aString))
-                    return true;
-            }
-        }
-        
-        
-        // if we get here, the string was not found in the commands
-        return false;
-    }
 
     //returns the main command of an alias, returns null if the command or alias don't exist
     public String Simplify(String str) {
