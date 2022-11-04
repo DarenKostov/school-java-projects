ee06d37d04b9aaeca955becfd7a9b8c47bcdc386
DarenKostov <daren.kostov@outlook.com>
2022-11-04 12:13:45 -0700

    sorting-project

---
+++ sorting/Main.java
@@ -0,0 +1,23 @@
+/*
+	Daren Kotsov
+	Sorting
+	11/04/2022
+	
+	
+ */
+
+
+
+
+import java.util.Scanner;
+
+public class Main {
+	
+
+	public static void main(String[] args) {
+	
+		System.out.println("works!");
+
+	}
+
+}
---
+++ sorting/Sort.java
@@ -0,0 +1,40 @@
+/*
+	Daren Kotsov
+    
+    class containgin several sorting algorithms
+    none of the function return anything
+    
+	11/04/2022
+	
+	
+ */
+
+
+
+
+
+public static class Sort{
+    
+    
+    //bubble sort
+    public static void Bubble(int[] in){
+    
+    }
+    
+    //selection sort
+    public static void Selection(int[] in){
+    
+    }
+    
+    //table sort
+    public static void Table(int[] in){
+    
+    }
+    
+    //quickshot sort
+    public static void Quickshot(int[] in){
+    
+    }
+
+
+}
