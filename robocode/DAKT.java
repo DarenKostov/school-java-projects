28f79b544672f75ced8b308fe0799903b05a8c2c
DarenKostov <daren.kostov@outlook.com>
2022-11-18 23:06:57 -0800

    todays work

--- robocode/DAKT.java
+++ robocode/DAKT.java
@@ -30,6 +30,8 @@ package dk;
 import robocode.*;
 import java.awt.Color;
 
+import java.lang.Math;
+
 
 //for debugging
 import java.awt.Graphics2D;
@@ -48,15 +50,35 @@ public class DAKT extends AdvancedRobot
 
 	int targetX=0;
 	int targetY=0;
-	int targetDX0=0;
-	int targetDY=0;
+	int targetDX=1;
+	int targetDY=1;
+	int targetDist=0;
+	
+	double rotation=0;
+	int rotationD=1;
+	
+	double nextAngleD=0;
+	double nextAngle=0;
 
 	public void onPaint(Graphics2D g) {
 	    // Set the paint color to red
 	    g.setColor(java.awt.Color.RED);
-	    // Paint a filled rectangle at (50,50) at size 100x150 pixels
+
+		//draw target		
 	    g.fillRect((int)(targetX+getX()), (int)(targetY+getY()), 10, 10);
+		
+		//draw target direction
+	    g.setColor(java.awt.Color.GREEN);
+	    g.fillRect((int)(targetX+getX()+targetDX), (int)(targetY+getY()+targetDY), 10, 10);
+		
+		//draw us
 	    g.fillRect((int)getX(), (int)getY(), 10, 10);
+		
+		
+		//draw where we think the target will be
+		
+	    g.setColor(java.awt.Color.ORANGE);
+	    g.fillRect((int)(Math.cos(nextAngle)*targetDist+getX()), (int)(Math.sin(nextAngle)*targetDist+getY()), 10, 10);
 	}
 
 
@@ -86,8 +108,12 @@ public class DAKT extends AdvancedRobot
 			// Replace the next 4 lines with any behavior you would like
 			//ahead(100);
 			//turnGunRight(360);
-			//back(100);
-			turnGunRight(5);
+			//back(100);i
+			if(rotation<0)
+				turnGunRight(Math.min(2,-rotationD));
+			else
+			
+				turnGunLeft(Math.min(1,rotationD));
 		}
 	}
 
@@ -98,8 +124,19 @@ public class DAKT extends AdvancedRobot
 	
 
 	
-		getRobotCoords(e);
-		scan();
+		// getRobotCoords(e);
+		// getRobotVel(e);
+		aimAngle(getRobotCoords(e),getRobotVel(e), 1000);
+		
+		
+		rotationD=200;
+
+		
+		
+		
+		
+		
+		//scan();
 		// Replace the next line with any behavior you would like
 		//fire(1);
 	}
@@ -122,6 +159,7 @@ public class DAKT extends AdvancedRobot
 	
 	//custom functions
 	
+	//grabs the targets coords
 	private double[] getRobotCoords(ScannedRobotEvent target){
 		double[] coords= new double[2];
 		
@@ -129,7 +167,7 @@ public class DAKT extends AdvancedRobot
 		double myDir=getGunHeadingRadians();
 		double tarDis=target.getDistance();
 		
-		
+		targetDist=(int)tarDis;
 		//X
 		coords[0]=Math.sin(myDir);
 		coords[0]*=tarDis;	
@@ -147,6 +185,72 @@ public class DAKT extends AdvancedRobot
 	
 	}
 	
+	//grabs the targets dx and xy
+	private double[] getRobotVel(ScannedRobotEvent target){
+		double[] d= new double[2];
+		
+		
+		double tarDir=target.getHeadingRadians();
+		double tarVel=target.getVelocity();
+		
+		
+		//X
+		d[0]=Math.sin(tarDir);
+		d[0]*=tarVel*5;	
+				
+		//Y
+		d[1]=Math.cos(tarDir);
+		d[1]*=tarVel*5;	
+		
+		targetDX=(int)d[0];
+		targetDY=(int)d[1];
+		
+		
+		return d;		
+		
+	
+	}
+	
+	
+	//sets angle of rotation
+
+//0 is X, 1 us Y
+private void aimAngle(double[] targetC, double[] targetD, double bulletSpeed) {
+    double rCrossV = targetC[0] * targetD[0] - targetC[1] * targetD[0];
+    double magR = Math.sqrt(targetC[0]*targetC[0] + targetC[1]*targetC[1]);
+    double angleAdjust = Math.asin(rCrossV / (bulletSpeed * magR));
+	
+	double angle= angleAdjust + Math.atan2(targetC[1], targetC[0]);
+	
+
+
+	nextAngleD=(angle/Math.PI*180);
+	nextAngle=angle;//(angle/Math.PI*180);
+//(int)getGunHeading();
+	
+	rotation=(nextAngle-getRotation());
+	
+	rotationD=(int)Math.toDegrees(rotation);
+
+
+	System.out.println("next:       "+nextAngle);
+	System.out.println("current:    "+getRotation());
+	System.out.println("difference: "+rotation);
+}
+	
+	
+	//get the rotation, does not go above pi*2 or bellow 0 
+	private double getRotation(){
+				
+		double output=getGunHeadingRadians();
+		
+		output%=(Math.PI*2);
+		
+		return output;
+
+
+
+	}
 	
 	
 	
