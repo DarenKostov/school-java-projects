58c69e6bcda358a814fc32abad438b0282157f39
DarenKostov <daren.kostov@outlook.com>
2023-04-28 09:39:10 -0700

    lanes

--- trafficSimulation/src/Road.java
+++ trafficSimulation/src/Road.java
@@ -1,2 +1,45 @@
-class Road{
+import java.awt.Graphics;
+import java.awt.Color;
+import javax.swing.JPanel;
+
+class Road extends JPanel{
+
+  int laneWidth=40;
+  int laneHeight=10;
+
+  int laneSpacingVertically=0;
+  int laneSpacingHorizontaly=0;
+
+  final int lanes=4;
+  final int laneSegments=20;
+  
+  public Road(){
+    super();
+
+    //700x1300 is the size of the window, getWidth() and getHeight() return in the constructor
+    laneSpacingVertically=700/(lanes);
+    laneSpacingHorizontaly=1300/laneSegments;    
+      
+  }
+
+
+  public void paintComponent(Graphics g){
+    super.paintComponent(g);
+
+    g.setColor(Color.BLACK);
+    g.fillRect(0, 0, getWidth(), getHeight());
+
+
+    g.setColor(Color.WHITE);
+    for(int i=0; i<lanes; i++){
+      for(int j=0; j<laneSegments; j++){
+        g.fillRect(j*laneSpacingHorizontaly, i*laneSpacingVertically+laneSpacingVertically, laneWidth, laneHeight);
+    
+      }
+    }
+    
+
+    
+  }
+  
 }
--- trafficSimulation/src/Traffic.java
+++ trafficSimulation/src/Traffic.java
@@ -1,10 +1,28 @@
+import javax.swing.JFrame;
+import java.awt.BorderLayout;
 
 
 class Traffic{
 
-    
+    JFrame frame = new JFrame("Traffic Simulation!");
+    Road road=new Road();
+
+    public Traffic(){
+        frame.setSize(1300, 700);
+        frame.setResizable(false);
+
+        frame.setLayout(new BorderLayout());
+        frame.add(road, BorderLayout.CENTER);
+        
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+
+
+        
+        
+    }
         
     public static void main(String[] args){
-
+        new Traffic();
     }
 }
